b21bd28a9119b93c16637088f837fd4a45f71bcb
Simon Dickson <simon@simonhdickson.com>
2018-06-20 20:57:35 +0100

    Add readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Queue Model Testing Example in Rust
+
+## Introduction
+
+This is an example of a model based test framework implemented in Rust.
+
+## Running
+
+```bash
+cargo test
+```
+
+The tests should fail with a list of commands that violate our test model,
+see if you can use the output to figure out what's wrong.
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,42 @@
 extern crate rand;
 
 use quickcheck::Gen;
-use std::fmt::{self,Debug};
 use rand::{ Rand};
 use quickcheck::{Arbitrary,empty_shrinker};
+
+// Generic Library Code
+
+trait Command<TActual, TModel> {
+    fn pre(&self, &TActual, &TModel) -> bool;
+    fn post(&self, &TActual, &TActual, &TModel, &TModel) -> bool;
+    fn model(&self, TModel) -> TModel;
+    fn actual(&mut self, TActual) -> TActual;
+}
+
+#[cfg(test)]
+fn confirm_model<TCommand, TActual: Clone, TModel: Clone>(commands: Vec<TCommand>, mut actual: TActual, mut model: TModel) -> bool
+where TCommand: Into<Box<Command<TActual, TModel>>> {
+    let mut passed = true;
+    for command in commands {
+        let mut command = command.into();
+        if !(*command).pre(&actual, &model) {
+            continue;
+        }
+        let old_queue = actual.clone();
+        let old_model = model.clone();
+        actual = (*command).actual(actual);
+        model = (*command).model(model);
+        if !(*command).post(&old_queue, &actual, &old_model, &model) {
+            passed = false;
+            break;
+        }
+    }
+
+    passed
+}
+
+// Queue Implementation
+
 
 #[derive(Clone)]
 struct Queue<T> {
@@ -38,89 +71,62 @@ impl<T> Queue<T> {
     }
 }
 
-trait Command<TActual, TModel> : Debug {
-    fn pre(&self, &TActual, &TModel) -> bool;
-    fn post(&self, &TActual, &TActual, &TModel, &TModel) -> bool;
-    fn model(&self, TModel) -> TModel;
-    fn actual(&mut self, TActual) -> TActual;
-}
+// Possible actions that can be taken in this model
 
-#[derive(Clone)]
 struct QueueReset {}
 
-impl Debug for QueueReset {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Reset")
-    }
-}
-
-impl Command<Queue<i32>, usize> for QueueReset {
-    fn pre(&self, _: &Queue<i32>, _: &usize) -> bool {
+impl<T : Clone> Command<Queue<T>, usize> for QueueReset {
+    fn pre(&self, _: &Queue<T>, _: &usize) -> bool {
         true
     }
-    fn post(&self, _: &Queue<i32>, actual: &Queue<i32>, _: &usize, model: &usize) -> bool {
+    fn post(&self, _: &Queue<T>, actual: &Queue<T>, _: &usize, model: &usize) -> bool {
         model == &actual.len()
     }
     fn model(&self, _: usize) -> usize {
         0
     }
-    fn actual(&mut self, mut actual: Queue<i32>) -> Queue<i32> {
+    fn actual(&mut self, mut actual: Queue<T>) -> Queue<T> {
         actual.reset();
         actual
     }
 }
 
-#[derive(Clone)]
 struct QueueGet<T> {
     actual_result: Option<T>
 }
 
-impl Debug for QueueGet<i32> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Get(actual_result:{:?})", self.actual_result)
-    }
-}
-
-impl Command<Queue<i32>, usize> for QueueGet<i32> {
-    fn pre(&self, _: &Queue<i32>, _: &usize) -> bool {
+impl<T: Clone> Command<Queue<T>, usize> for QueueGet<T> {
+    fn pre(&self, _: &Queue<T>, _: &usize) -> bool {
         true
     }
-    fn post(&self, _: &Queue<i32>, actual: &Queue<i32>, old_model: &usize, model: &usize) -> bool {
-        model == &actual.len() && (if old_model >= &1 { self.actual_result != None } else { true })
+    fn post(&self, _: &Queue<T>, actual: &Queue<T>, old_model: &usize, model: &usize) -> bool {
+        model == &actual.len() && (if old_model >= &1 { self.actual_result.is_some() } else { true })
     }
     fn model(&self, model: usize) -> usize {
         if model == 0 { 0 } else { model - 1 }
     }
-    fn actual(&mut self, mut actual: Queue<i32>) -> Queue<i32> {
+    fn actual(&mut self, mut actual: Queue<T>) -> Queue<T> {
         self.actual_result = actual.get();
         actual
     }
 }
 
-#[derive(Clone)]
 struct QueuePush<T> {
     input_value: T
 }
 
-impl Debug for QueuePush<i32> {
-fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "Push(input_value:{})", self.input_value)
-        }
-}
-
-
-impl Command<Queue<i32>, usize> for QueuePush<i32> {
-    fn pre(&self, _: &Queue<i32>, _: &usize) -> bool {
+impl<T: Clone> Command<Queue<T>, usize> for QueuePush<T> {
+    fn pre(&self, _: &Queue<T>, _: &usize) -> bool {
         true
     }
-    fn post(&self, _: &Queue<i32>, actual: &Queue<i32>, _: &usize, model: &usize) -> bool {
+    fn post(&self, _: &Queue<T>, actual: &Queue<T>, _: &usize, model: &usize) -> bool {
         model == &actual.len()
     }
     fn model(&self, model: usize) -> usize {
         model + 1
     }
-    fn actual(&mut self, mut actual: Queue<i32>) -> Queue<i32> {
-        actual.push(self.input_value);
+    fn actual(&mut self, mut actual: Queue<T>) -> Queue<T> {
+        actual.push(self.input_value.clone());
         actual
     }
 }
@@ -131,6 +137,9 @@ enum QueueCommand<T> where T: Rand {
     Push(T),
     Reset
 }
+
+// Arbitrary/Random implementations that allow the above commands to be
+// randomly generated
 
 impl<T: Arbitrary + Rand> Arbitrary for QueueCommand<T> {
     fn arbitrary<G: Gen>(g: &mut G) -> Self { g.gen() }
@@ -144,8 +153,9 @@ impl<T: Arbitrary + Rand> Arbitrary for QueueCommand<T> {
     }
 }
 
-impl Into<Box<Command<Queue<i32>, usize>>> for QueueCommand<i32> {
-    fn into(self) -> Box<Command<Queue<i32>, usize>> {
+impl<T: Arbitrary + Rand, TModel> Into<Box<Command<Queue<T>, TModel>>> for QueueCommand<T>
+where QueuePush<T>: Command<Queue<T>, TModel>, QueueReset: Command<Queue<T>, TModel>, QueueGet<T>: Command<Queue<T>, TModel> {
+    fn into(self) -> Box<Command<Queue<T>, TModel>> {
         match self {
             QueueCommand::Get       => Box::new(QueueGet{actual_result: None}),
             QueueCommand::Push(val) => Box::new(QueuePush{input_value: val}),
@@ -157,29 +167,15 @@ impl Into<Box<Command<Queue<i32>, usize>>> for QueueCommand<i32> {
 fn main() {
 }
 
+// We can run this model against any QueueCommand<T> where T implements the Rand trait
+// I choose unit and i32 simply because they already implement it.
+
 #[cfg(test)]
 quickcheck! {
-    fn prop(commands: Vec<QueueCommand<i32>>) -> bool {
-        let mut queue: Queue<i32> = Queue::new();
-        let mut model = 0;
-        let mut passed = true;
-        for command in commands {
-            let mut command: Box<Command<Queue<i32>, usize>> = command.into();
-            if !(*command).pre(&queue, &model) {
-                continue;
-            }
-            let old_queue = queue.clone();
-            let old_model = model.clone();
-            queue = (*command).actual(queue);
-            model = (*command).model(model);
-            if !(*command).post(&old_queue, &queue, &old_model, &model) {
-                passed = false;
-                break;
-            }
-        }
-
-        assert!(passed, "oh no");
-        
-        true
+    fn prop_i32(commands: Vec<QueueCommand<i32>>) -> bool {
+        confirm_model(commands, Queue::new(), 0)
+    }
+    fn prop_unit(commands: Vec<QueueCommand<()>>) -> bool {
+        confirm_model(commands, Queue::new(), 0)
     }
 }
